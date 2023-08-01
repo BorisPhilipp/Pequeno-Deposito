d65537448b1c25dd8250b86f6d4413d6cdb95321
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2023-07-31 21:25:15 -0300

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,49 @@
+'''
+Operadores Aritméticos
+Adição -> +
+Subtração -> -
+Multiplicação -> *
+Divisão -> /
+Exponenciação -> **
+Parte Inteira -> //
+Módulo -> %
+'''
+
+#Entrada e Saida
+print("Digite seu nome:")
+nome = input()
+print("Bem-Vindo(a), ", nome)
+PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
+'''
+Operadores de Comparação
+== -> Igualdade
+!= - Diferente
+> -> Maior
+< -> Menor
+<= -> menor ou Igual
+'''
+
+#Estruturas condicionais Simples
+a = 6
+b = 4
+soma = a + b
+
+if soma > 10 :
+    print("O valor da soma é: ",soma)
+else:
+    print("valor falso do teste")
+
+'''
+Operadores Lógicos
+AND -> e 
+OR -> ou
+NOT -> não
+'''
+
+x = 3.5
+y = 1.2
+
+if y < x and x > 3:
+    print("as duas condições são verdadeiras")
+else:
+    print("pelo menos uma condição ou as duas são falsa")
