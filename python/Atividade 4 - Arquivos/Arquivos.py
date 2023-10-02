036c8485de2de0f7f62893768979b43827c876de
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2023-10-02 12:34:07 -0300

    Update Arquivos.py

--- python/Atividade 4 - Arquivos/Arquivos.py
+++ python/Atividade 4 - Arquivos/Arquivos.py
@@ -1,52 +1,46 @@
 import random
-"""
-def escreverNumerosAleatorios(qtdNumeros, nomeArquivo):
-    arquivoNumeros = open(nomeArquivo, 'w')
-    for i in range(qtdNumeros):
-        arquivoNumeros.write(str(random.randint(0,100)))
-        arquivoNumeros.write("\n")
-
-    arquivoNumeros.close()
-
-escreverNumerosAleatorios(100, 'aleatorios.txt')
-
-def escreverMedia(qtdNumeros, nomeArquivo):
-    arquivoNumeros = open(nomeArquivo)
-    soma = 0
-
-    for i in range(qtdNumeros):
-        num = float(arquivoNumeros.readline())
-        soma += num
-    arquivoNumeros.close()
-    return soma/qtdNumeros
-print(escreverMedia(3, 'media.txt'))
-"""
-"""
-def copiaArquivo(velhoArquivo, novoArquivo):
-    f1 = open(velhoArquivo, 'r')
-    f2 = open(novoArquivo, 'w')
-    for texto in f1:
-        f2.write(texto)
-    f1.close()
-    f2.close()
-
-copiaArquivo('media.txt', 'novoMedia.txt')
-"""
 # Exercicio - 1
-Nomes = ["Talion","Smeagol","Gollum","Celebrimbor","Sauron","Bilbo","Frodo","Gandalf","Gimli","Aragorn"]
-Sobrenomes = ["O Nunca-Morto","O Banido","O Assassino","O Ferreiro-Elfo","Senhor do Escuro","Bolseiro","Bolseiro","O Mago Branco","O Anao","O Rei de Gondor"]
+"""
+1.Faça um programa que leia um número N e gere um arquivo com N nomes e idades aleatórios
+⦁ Faça uso de duas listas criadas na mão: uma que contenha 20 nomes e outra que contenha 20 sobrenomes
+⦁ Cada linha do arquivo resultante deve conter um nome completo e a sua idade"""
 
-def escreverNomes(Nomes, nomeArquivo):
+Nomes = ["Talion","Smeagol","Gollum","Celebrimbor","Sauron","Bilbo","Frodo","Gandalf","Gimli","Aragorn","Legolas","Thorin","Saruman","Galadriel","Elrond","Pippin","Samwise Gamgee","Boromir","Faramir","Radagast","Merry"]
+Sobrenomes = ["O Nunca-Morto","O Banido","O Assassino","O Ferreiro-Elfo","Senhor do Escuro","Bolseiro","Bolseiro","O Mago Branco","O Anao","O Rei de Gondor","O Elfo Arqueiro","Ultimo Herdeiro de Durin","Ultimo Mago Branco","A Mais Pura e Justa","Rei dos Elfos da Floresta","O Peregrino Tolo","Melhor Amigo de Frodo","O Humano","Irmao de Boromir","O Castanho","Amigo de Pippin"]
+digit = int(input("Digite a quantidade de Soldados que você precisa. (0 - 20): "))
+
+def escreverNomes(nomeArquivo):
     arquivoNomes = open(nomeArquivo, 'w')
-    arquivoSobrenomes = open(nomeArquivo, 'a')
-    for i in Nomes:
-        arquivoNomes.write(str(i))
-        arquivoNomes.write("\n")
+    arquivoNomes.write(f'{"NOMES":<17} {"SOBRENOMES":<23} {"IDADES":>8}\n')
 
-    for i in Sobrenomes:
-        arquivoSobrenomes.write(str(i))
-        arquivoSobrenomes.write("\n")
+    for i in range(digit):
+        aleatorio = random.randint(0,20)
+        aleatorioSobre = random.randint(0,20)
+        SobrenomesA = str(Sobrenomes[aleatorioSobre])
+        nomesA = str(Nomes[aleatorio])
+        idades = random.randint(0,100)
+        terraMedia = f"{nomesA:<17} {SobrenomesA:<23} {idades:>8}\n"
+        arquivoNomes.write(terraMedia)
     arquivoNomes.close()
 
-escreverNomes(Nomes, 'nomes.txt')
 
+# Exercicio - 2
+"""2.Estenda o exemplo do cadastro para considerar também a altura da pessoa"""
+
+def Alturas(nomeArquivo):
+    arquivoNomes = open(nomeArquivo, 'a')    
+    arquivoNomes.write("\n")
+    arquivoNomes.write("ALTURAS")
+    arquivoNomes.write("\n")
+
+    for i in range(digit):                  #For que faz aparecer alturas aleatorias
+        arquivoNomes.write(str(round(random.uniform(1.00, 2.10),2)))
+        arquivoNomes.write(str("\n"))
+    arquivoNomes.close()
+escreverNomes('nomes.txt')
+
+choice = input("Deseja adicionar as alturas?(S/N): ").lower()
+if choice == 's':
+    Alturas('nomes.txt')
+else:
+    exit()
