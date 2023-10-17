7d58d82a06988269952869eae8f5f30a37fed3df
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2023-10-16 21:59:02 -0300

    Update veiculos.py

--- python/Atividade 5/veiculos.py
+++ python/Atividade 5/veiculos.py
@@ -3,6 +3,9 @@ class Veiculo:
         self._tipo = tipo
         self._velocidade = velocidade
 
+class Descricao:
+    print("É um Automóvel automatizado de extrema automatização")
+
 class Car(Veiculo):
     def __init__(self, tipo, velocidade):
         super().__init__(tipo, velocidade)
