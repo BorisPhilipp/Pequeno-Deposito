5bbbc51649c942a2355585d6aaacf80aa7f8d6aa
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2023-10-22 23:16:29 -0300

    mudança de pastas

--- Python/Professor.py
+++
@@ -1,9 +0,0 @@
-from Pessoa_abc import Pessoa
-
-class Professor(Pessoa):
-    def __init__(self, nome):
-        super().__init__(nome)
-        self._nome = nome
-    def saudar(self):
-        return f'Olá, eu sou o prof {self._nome}'
-
